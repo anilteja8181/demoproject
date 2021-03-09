6a7fa9e3bdbbb9209ee74ccf4f9e90ff6568f3da
Anil Teja Yadagani (iDEAS-Cloud Transformation - iDEAS-Cloud Transformation) <an20169515@wipro.com>
2021-03-09 19:33:06 +0530

    Add project files.

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+
+
+ReactDOM.render(
+<>
+<div class="centre">
+<h1> Mern application which triggers rest api build in jenkins
+</h1>
+</div>
+
+<a href='http://anil:111028ccdc84f6a8d77316f8a7e2e0f8d3@23.251.159.27:8080/job/demo/build'>
+<button>
+Build
+</button>
+</a>
+</>,
+document.getElementById('root'));
